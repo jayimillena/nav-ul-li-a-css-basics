6a6a87e9d90473884a9906830894f28ccffd05af
Jay Millena <jay.millena07@gmail.com>
2023-11-22 10:04:01 +0800

    Initial commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about-us.php">About us</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My First Potfolio</title>
+  <link href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+  <?php include('header.php') ?>
+  <img src="images/Forest by Sebastian Unrau.jpg" width="500px" alt="">
+  <h1>Home page</h1>
+</body>
+</html>
